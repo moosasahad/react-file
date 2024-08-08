eb7a4bb0a76007fcf9754613e83b8c95e174b986
moosa sahad <sahadmoosa018@gmail.com>
2024-08-09 00:40:02 +0530

    creted a class componenet

---
+++ src/firstcomponent.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+export class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello from MyComponent!</h2>
+      </div>
+    );
+  }
+}
